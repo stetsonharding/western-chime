3d7d2cbe2c9056dc8326462beaadd79de4a5384b
stetsonharding <stetson2001@msn.com>
2022-09-28 07:57:08 -0700

    Media queries for all device sizes -Favicon added.

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -9,7 +9,7 @@ function Header() {
     <div className="header-container">
       <div className="header-slogan-container">
         <h1 className="header-title slogan">Western Chime</h1>
-        <h3 className="slogan">The Saloon with a Kick.</h3>
+        <h3 className="header-subtitle slogan">The Saloon for cowboys.</h3>
       </div>
       <div className="shopping-cart">
         <img src={ShoppingCart} height="50" width="50" alt="Shopping Cart" />
