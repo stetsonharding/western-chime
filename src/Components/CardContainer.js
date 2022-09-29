edae71f4ea5ea1ffdbdeee0a658ceff840d28c53
stetsonharding <stetson2001@msn.com>
2022-09-29 07:18:06 -0700

    Card Container prev and next buttons.

--- src/Components/CardContainer.js
+++ src/Components/CardContainer.js
@@ -2,8 +2,23 @@ import React from "react";
 
 import "../css/CardContainer.css";
 
+import nextButton from "../Assets/nextbutton.png";
+import prevButton from "../Assets/prevbutton.png";
+
 function CardContainer() {
-  return <div className="card-container"></div>;
+  return (
+    <div className="card-container">
+      <div className="page-button-container">
+        <span className="page-buttons">
+          <img src={prevButton} alt="Previous Page" />
+        </span>
+
+        <span className="page-buttons">
+          <img src={nextButton} alt="Next Page" />
+        </span>
+      </div>
+    </div>
+  );
 }
 
 export default CardContainer;
