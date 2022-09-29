4ee24bb9c8efc40a6e02034eac31328e3326bf55
stetsonharding <stetson2001@msn.com>
2022-09-28 19:35:12 -0700

    Card container layout and styles.

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import "./App.css";
 
 //Components
 import Header from "./Components/Header";
+import CardContainer from "./Components/CardContainer";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <CardContainer />
     </div>
   );
 }
---
+++ src/Components/CardContainer.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+import "../css/CardContainer.css";
+
+function CardContainer() {
+  return <div className="card-container"></div>;
+}
+
+export default CardContainer;
