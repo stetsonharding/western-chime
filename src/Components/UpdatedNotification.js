7f934fd5e0fc7ca0713bd0258266f4b52f371797
stetsonharding <stetson2001@msn.com>
2022-12-25 17:56:15 -0800

    updated notification compoent

---
+++ src/Components/UpdatedNotification.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function UpdatedNotification(props) {
+  return (
+    <div className="quantity-confirm">
+      <h4>{props.notification}</h4>
+    </div>
+  );
+}
+
+export default UpdatedNotification;
