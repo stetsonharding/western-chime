a064a3674ded9b486b35fce1baae10df1f142f3d
stetsonharding <stetson2001@msn.com>
2023-01-25 07:50:58 -0800

    edit modal header

--- src/Components/EditItemModal.js
+++ src/Components/EditItemModal.js
@@ -1,20 +1,20 @@
 import React from "react";
 
+import "../css/EditCartItemModal.css";
 function EditItemModal({ setEditedCartItem, editedCartItem }) {
   return (
     <div className="modal-background">
-      <div
-        style={{ width: "500px", height: "500px" }}
-        className="modal-content"
-      >
-        {editedCartItem.name}
-        <button
-          onClick={() => {
-            setEditedCartItem({});
-          }}
-        >
-          close
-        </button>
+      <div className="modal-content modal-size">
+        <div className="editModal-header">
+          <button
+            className="close-button"
+            type="button"
+            onClick={() => setEditedCartItem({})}
+          >
+            &#10006;
+          </button>
+          <h3 className="modal-title">Edit Order</h3>
+        </div>
       </div>
     </div>
   );
