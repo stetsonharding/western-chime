374395f45ceea7eee27a3cea8c197933657263b1
stetsonharding <stetson2001@msn.com>
2023-01-26 07:21:26 -0800

    Edit modal hydration

--- src/Components/EditItemModal.js
+++ src/Components/EditItemModal.js
@@ -4,16 +4,34 @@ import "../css/EditCartItemModal.css";
 function EditItemModal({ setEditedCartItem, editedCartItem }) {
   return (
     <div className="modal-background">
-      <div className="modal-content modal-size">
-        <div className="editModal-header">
-          <button
-            className="close-button"
-            type="button"
-            onClick={() => setEditedCartItem({})}
+      <div className="edit-item-modal">
+        <div className="">
+          <div
+            className="edit-cart-item-header"
+            style={{ display: "flex", justifyContent: "space-between" }}
           >
-            &#10006;
-          </button>
-          <h3 className="modal-title">Edit Order</h3>
+            <button
+              onClick={() => setEditedCartItem({})}
+              type="button"
+              aria-label="edit"
+            >
+              X
+            </button>
+            {/* <h3>Update Cart</h3> */}
+          </div>
+          <h2 className="edit-item-title">{editedCartItem.name}</h2>
+
+          <div className="edit-item-information">
+            <img
+              src={editedCartItem.image_url}
+              alt="Beverage"
+              width="40px"
+              height="150px"
+            />
+            <h5>
+              {editedCartItem.qty} @ ${editedCartItem.srm.toFixed(2)}
+            </h5>
+          </div>
         </div>
       </div>
     </div>
